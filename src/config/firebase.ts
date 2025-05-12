2d8a35cf62bb4e7f6ad0b76c78d0dd135ce5dde1
Sean Hwang <seanphwang@gmail.com>
2025-05-12 15:57:09 -0400

    trigger build again

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,6 +6,7 @@ dotenv.config();
 
 // Log environment variables for debugging
 console.log('Firebase Project ID:', process.env.FIREBASE_PROJECT_ID);
+console.log('trigger again');
 
 // Initialize Firebase Admin
 // Note: You'll need to set up your Firebase service account credentials
