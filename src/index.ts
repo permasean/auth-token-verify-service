9f76f3ffc1f976f488d0b0a4376190700fb936a7
Sean Hwang <seanphwang@gmail.com>
2025-05-12 16:00:19 -0400

    use 8080 for port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { auth } from './config/firebase';
 import { VerifyTokenRequest, VerifyTokenResponse } from './types/auth';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Middleware for parsing JSON bodies
 app.use(express.json());
