5518d5423b385e4595f749020dc82060b44bf31a
Sean Hwang <seanphwang@gmail.com>
2025-05-12 16:25:50 -0400

    adjust firebase configs

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -12,18 +12,10 @@ console.log('trigger again');
 // Note: You'll need to set up your Firebase service account credentials
 // You can either use environment variables or a service account JSON file
 if (!admin.apps.length) {
-  if (!process.env.FIREBASE_PROJECT_ID) {
-    throw new Error('FIREBASE_PROJECT_ID is not set in environment variables');
-  }
-
   const appConfig: admin.AppOptions = {
     credential: admin.credential.applicationDefault()
   };
-
-  if (process.env.FIREBASE_PROJECT_ID) {
-    appConfig.projectId = process.env.FIREBASE_PROJECT_ID;
-  }
-
+  
   admin.initializeApp(appConfig);
   
   console.log('Firebase Admin initialized successfully');
